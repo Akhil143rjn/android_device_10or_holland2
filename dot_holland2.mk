e202f6c91adde41d3cbc1cafed27b1aeb68bb63f
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2021-11-04 08:49:17 +0530

    Holland2: update fingerprint to redfin october

--- dot_holland2.mk
+++ dot_holland2.mk
@@ -45,9 +45,9 @@ BOARD_VENDOR := 10or
 PRODUCT_GMS_CLIENTID_BASE := android-10or
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/coral/coral:11/RQ3A.210905.001/7511028:user/release-keys
+    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
